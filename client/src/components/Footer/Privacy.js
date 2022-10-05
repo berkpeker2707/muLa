936ac4e0b8a0e1eb24407416a2bc9b595baa510d
Berk <berkpeker94@gmail.com>
2022-10-05 17:46:52 +0300

    changed privacy text

--- client/src/components/Footer/Privacy.js
+++ client/src/components/Footer/Privacy.js
@@ -5,12 +5,10 @@ export default function Privacy() {
   return (
     <div className="FAQComponent">
       <ul className="FAQList">
-        <h4 className="FAQListHeader">Frequently Asked Questions</h4>
+        <h4 className="FAQListHeader">Privacy Policy</h4>
         <hr className="FAQListHeaderLine" />
         <br />
-        <li className="FAQListElement">
-          * How does muLa calculate my Myer Briggs Type?
-        </li>
+        <li className="FAQListElement">* Why do we value privacy?</li>
         <p>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
           eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
@@ -20,45 +18,20 @@ export default function Privacy() {
           pariatur.{" "}
         </p>
         <br />
-        <li className="FAQListElement">
-          * Do I really need to complete muLa's test?
-        </li>
+        <li className="FAQListElement">* Why we ensure our transparency?</li>
         <p>
           Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
           officia deserunt mollit anim id est laborum.
         </p>
-        <br />
-        <li className="FAQListElement">
-          * How much information muLa keeps and why?
-        </li>
+        <li className="FAQListElement">* The Information You Give</li>
         <p>
-          Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-          accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae
-          ab illo inventore veritatis et quasi architecto beatae vitae dicta
-          sunt explicabo.
+          Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
+          officia deserunt mollit anim id est laborum.
         </p>
-        <br />
-        <li className="FAQListElement">* Can I re-do my test at any time?</li>
+        <li className="FAQListElement">* Why we ensure our transparency?</li>
         <p>
-          Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet,
-          consectetur, adipisci velit, sed quia non numquam eius modi tempora
-          incidunt ut labore et dolore magnam aliquam quaerat voluptatem.{" "}
-        </p>
-        <br />
-        <li className="FAQListElement">
-          * Can I change my personal information?
-        </li>
-        <p>
-          Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut
-          fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem
-          sequi nesciunt.
-        </p>
-        <br />
-        <li className="FAQListElement">* How can I delete my muLa account</li>
-        <p>
-          Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet,
-          consectetur, adipisci velit, sed quia non numquam eius modi tempora
-          incidunt ut labore et dolore magnam aliquam quaerat voluptatem.
+          Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
+          officia deserunt mollit anim id est laborum.
         </p>
         <br />
       </ul>
